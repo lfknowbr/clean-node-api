25449932dc9a51352afbc0754f5bc001a625c295
Luis fernando <fernandocpdoba@gmail.com>
2019-12-03 16:32:26 -0300

    ensure LoginRouter return valid token when valid credentials are provided

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -22,9 +22,10 @@ module.exports = class HttpResponse {
     }
   }
 
-  static ok () {
+  static ok (accessToken) {
     return {
-      statusCode: 200
+      statusCode: 200,
+      body: accessToken
     }
   }
 }
