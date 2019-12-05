5e5128e6a05f0a38d40396c27608cbe1614c1515
Luis fernando <fernandocpdoba@gmail.com>
2019-12-05 14:31:46 -0300

    ensure LoginRouter return 500 if EmailValidator throws

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -28,6 +28,15 @@ const makeEmailValidatorSpy = () => {
   return emailValidatorSpy
 }
 
+const makeEmailValidatorWithError = () => {
+  class EmailValidatorCaseSpy {
+    isValid () {
+      throw new Error()
+    }
+  }
+  return new EmailValidatorCaseSpy()
+}
+
 const makeAuthUseCase = () => {
   class AuthUseCaseSpy { // class mock data
     async auth (email, password) {
@@ -209,5 +218,19 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 500 if EmailValidator throws', async () => {
+    const authUseCaseSpy = makeAuthUseCase()
+    const emailValidatorSpy = makeEmailValidatorWithError()
+    const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy)
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
